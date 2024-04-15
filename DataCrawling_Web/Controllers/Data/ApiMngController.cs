0cd96330c02bca57248b1115e433007114189a04
유지혁 <lasidos@naver.com>
2024-04-16 02:20:43 +0900

    View , Controller 연결

---
+++ DataCrawling_Web/Controllers/Data/ApiMngController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace DataCrawling_Web.Controllers.Data
+{
+    public class ApiMngController : Controller
+    {
+        [Route("Data/ApiMng/List")]
+        public ActionResult List()
+        {
+            return View("~/Views/Data/ApiMng/List.cshtml");
+        }
+
+        [Route("Data/ApiMng/Scrap")]
+        public ActionResult Scrap()
+        {
+            return View("~/Views/Data/ApiMng/Scrap.cshtml");
+        }
+
+        [Route("Data/ApiMng/AutoWeb")]
+        public ActionResult AutoWeb()
+        {
+            return View("~/Views/Data/ApiMng/AutoWeb.cshtml");
+        }
+
+        [Route("Data/ApiMng/AutoWork")]
+        public ActionResult AutoWork()
+        {
+            return View("~/Views/Data/ApiMng/AutoWork.cshtml");
+        }
+    }
+}
