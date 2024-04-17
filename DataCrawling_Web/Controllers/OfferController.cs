ea5b539a50ff7fe4f1f846b20d73dea6365bf553
유지혁 <lasidos@naver.com>
2024-04-18 01:55:18 +0900

    의뢰 약관 업데이트

--- DataCrawling_Web/Controllers/OfferController.cs
+++ DataCrawling_Web/Controllers/OfferController.cs
@@ -1,36 +1,72 @@
-﻿using DataCrawling_Web.BSL.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using DataCrawling_Web.BSL.Authentication;
+using DataCrawling_Web.BSL.Common;
 using System.Web.Mvc;
-using System.Web.Services;
+using System.Web.UI.WebControls;
+using static System.Windows.Forms.AxHost;
 
 namespace DataCrawling_Web.Controllers
 {
     public class OfferController : Controller
     {
-        // GET: RequestLetter
         public ActionResult List(string menu)
         {
-            string subject = "";
+            string subject = string.Empty, type = string.Empty;
             switch (menu)
             {
                 case "scrap":
+                    type = "S";
                     subject = "스크랩핑";
                     break;
                 case "prs":
+                    type = "P";
                     subject = "웹·업무 자동화";
                     break;
             }
             if (subject == "") return Content(Commons.AlertMessage("잘못된 접근입니다."));
 
+            ViewBag.State = "접수대기";
             ViewBag.Subject = subject;
+            ViewBag.type = type;
             return View();
         }
 
-        public ActionResult Terms()
+        [HttpPost]
+        public ActionResult GetTabData(string Tab)
         {
+            string state = string.Empty;
+            switch (Tab)
+            {
+                case "0":
+                    state = "접수대기";
+                    break;
+                case "1":
+                    state = "진행중";
+                    break;
+                case "2":
+                    state = "완료";
+                    break;
+            }
+            ViewBag.State = state;
+            return PartialView("~/Views/Offer/_PartialView/_ListItem.cshtml");
+        }
+
+        public ActionResult Terms(string type)
+        {
+            string title = string.Empty;
+            switch (type)
+            {
+                case "S":
+                    title = "스크랩핑";
+                    break;
+                case "P":
+                    title = "웹·업무 자동화";
+                    break;
+            }
+            string termsofservice = string.Join("\n", Utility.ReadAllText(Server.MapPath("~/Resource/Text/termsofservice.txt")));
+            string privacy = string.Join("\n", Utility.ReadAllText(Server.MapPath("~/Resource/Text/privacy.txt")));
+            ViewBag.title = title;
+            ViewBag.termsofservice = termsofservice;
+            ViewBag.privacy = privacy;
             return View();
         }
 
--- DataCrawling_Web/Resource/Js/Offer/List.js
+++ DataCrawling_Web/Resource/Js/Offer/List.js
@@ -1,3 +1,13 @@
 ﻿$(function () {
     $('.modal').removeClass('active');
+
+    $('.mtcTplTab .tabItems li').click(function () {
+        $('.mtcTplTab .tabItems li').removeClass('on');
+        $(this).addClass('on');
+
+        $('.listWrap').load("/Offer/GetTabData",
+            {
+                Tab: $(this).val()
+            });
+    });
 });
