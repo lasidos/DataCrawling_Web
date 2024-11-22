070b3337d1912d639b701543ad0fb89c3ff094ad
lasidos <lasidos@PI-2-LASIDOS>
2024-11-22 18:05:40 +0900

    API 페이지 추가

---
+++ DataCrawling_Web/Controllers/Admin/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataCrawling_Web.Controllers.Admin
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult API()
+        {
+            return View("~/Views/Admin/Service/API.cshtml");
+        }
+
+        public ActionResult Automation()
+        {
+            return View("~/Views/Admin/Service/Automation.cshtml");
+        }
+    }
+}
--- DataCrawling_Web/Models/Data/DataInfoModel.cs
+++ DataCrawling_Web/Models/Data/DataInfoModel.cs
@@ -33,8 +33,6 @@ namespace DataCrawling_Web.Models.Data
         public string KEYWORD { get; set; }
         public string D_LINK { get; set; }
         public string R_TYPE { get; set; }
-        public int PRICE { get; set; }
-        public int PRICE_STANDARD { get; set; }
         public DateTime CREATE_DT { get; set; }
         public DateTime EDIT_DT { get; set; }
         public int READ_CNT { get; set; }
